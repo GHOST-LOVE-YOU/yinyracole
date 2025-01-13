b639ab358630e727c9cbf43a4c9b62c34144c21f
ghost-love-you <1756613825@qq.com>
2025-01-13 22:43:46 +0800

    sample blog demo
    source code:https://github.com/vercel/examples/blob/main/solutions/blog/

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import Footer from "@/components/footer";
+import { Navbar } from "@/components/nav";
+
+import { baseUrl } from "./sitemap";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,8 +18,31 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Yinyra Cole",
-  description: "Yinyra Cole`s blog",
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: "Next.js Portfolio Starter",
+    template: "%s | Next.js Portfolio Starter",
+  },
+  description: "This is my portfolio.",
+  openGraph: {
+    title: "My Portfolio",
+    description: "This is my portfolio.",
+    url: baseUrl,
+    siteName: "My Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
@@ -23,11 +51,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className="bg-white text-black dark:bg-black dark:text-white"
+    >
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} mx-4 mt-8 max-w-xl antialiased lg:mx-auto`}
       >
-        {children}
+        <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
+          <Navbar />
+          {children}
+          <Footer />
+          {/* <Analytics /> */}
+          {/* <SpeedInsights /> */}
+        </main>
       </body>
     </html>
   );
