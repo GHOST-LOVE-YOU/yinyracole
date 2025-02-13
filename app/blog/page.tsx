c20ea3579f106deabcde005777af122e71212572
ghost-love-you <1756613825@qq.com>
2025-02-13 20:17:51 +0800

    [process][react] Module 01-05

---
+++ app/blog/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const BlogList = () => {
+  return (
+    <ul>
+      <li>
+        <a href="/blog/posts/00-readme">00 readme</a>
+      </li>
+      <li>
+        <a href="/blog/posts/01-start-from-js">01 start from JS</a>
+      </li>
+      <li>
+        <a href="/blog/posts/02-understanding-jsx">02 understanding JSX</a>
+      </li>
+      <li>
+        <a href="/blog/posts/03-expression-slots">03 expression Slots</a>
+      </li>
+      <li>
+        <a href="/blog/posts/04-differences-from-html">
+          04 differences from html
+        </a>
+      </li>
+      <li>
+        <a href="/blog/posts/05-the-whitespace-gotcha">
+          05 The Whitespace Gotcha
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default BlogList;
